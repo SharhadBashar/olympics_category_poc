3863eee64f851bf41624635c5d3a74c1aabf2355
Sharhad Bashar <sharhad.bashar@Belal-Koura.targetspot.ny>
2023-03-13 17:22:27 -0400

    first commit

---
+++ python/category.py
@@ -0,0 +1,5 @@
+class Category:
+    def __init__(self, text_file, language = 'en', text_data_path = None):
+        self.category_data_path = '../data/words/'
+        
+        
---
+++ python/main.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import time
+
+from att import Audio_To_Text
+from category import Category
+
+try:
+    language = sys.argv[1].lower()
+except IndexError:
+    print('No language specified. Please pass -en for English or -fr for French')
+    exit()
+
+try:
+    audio_file = sys.argv[2].lower()
+except IndexError:
+    print('No audio found. Please pass an audio file')
+    exit()
+    
+if (language == '-e'):
+    start = time.time()
+    Audio_To_Text(audio_file, language = 'en')
+    print('Finished transcribing in {} s'.format(round(time.time() - start, 2)))
+    Category(text_file = audio_file.split('.')[0] + '_en.pkl', language = 'en')
+    
+elif (language == '-f'):
+    start = time.time()
+    Audio_To_Text(audio_file, language = 'fr')
+    print('Finished transcribing in {} s'.format(round(time.time() - start, 2)))
+    Category(text_file = audio_file.split('.')[0] + '_fr.pkl', language = 'en')
+    
+else:
+    print('Unknown language. Please pass -en for English or -fr for French')
+    exit()
